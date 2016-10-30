a402d44490e7c1a4a617edb075a964534de03a47
MuthuKrishnanOfficial <MuthuKrishnanOfficial@gmail.com>
2016-10-30 17:44:33 +0530

    Create randomcolor.php

---
+++ randomcolor.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Don't Remove this comment!.
+  
+  Random Material Color Generator
+  Input: none
+  Output: Hex code of color in string type
+  Conditions: same color should not repeat on consecutive call of function
+ 
+ 
+*/
+
+ //Write your php function below.
+
+
+?>
