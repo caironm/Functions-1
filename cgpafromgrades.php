5f4954c1a73f9e87c589f9a3912f96e57276bb1b
Muthu Krishnan <MuthuKrishnanOfficial@gmail.com>
2016-10-30 18:55:40 +0530

    Create cgpafromgrades.php

---
+++ cgpafromgrades.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* 
+    Don't Remove this comment!. 
+    
+    Anna University CGPA Calc from Grades
+    Input: all subjects grades upto n sems, department in short code (eg ECE)
+    Output: A float num of cgpa with 4 values after decimal
+    Condition: Must be secure Your can create as many as auxillary functions you need. 
+  
+*/ 
+
+
+// Write your php fucntion below
+
+?>
