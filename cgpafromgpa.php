ee112908fa61050f745e56d2bbdb164c6488f51c
Muthu Krishnan <MuthuKrishnanOfficial@gmail.com>
2016-10-30 20:54:54 +0530

    Update cgpafromgpa.php

--- cgpafromgpa.php
+++ cgpafromgpa.php
@@ -7,9 +7,16 @@
     Input: GPA of n sem, department in short code (eg ECE)
     Output: A float num of cgpa with 4 values after decimal
     Condition: Must be secure Your can create as many as auxillary functions you need. 
-  
+    
 */
 
+/*
+Update on problem:
+    assume the gpa's are in a array
+    $_POST['gpa']
+    so $_POST['gpa'][0] has 1st sem gpa.. etc.,
+
+*/
 
 // Write your php fucntion below
 
