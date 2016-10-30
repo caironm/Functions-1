0a29b7e871ab375f6f21ba94870cb3eaf3098cae
Muthu Krishnan <MuthuKrishnanOfficial@gmail.com>
2016-10-30 19:01:59 +0530

    Create spamcheck.php

---
+++ spamcheck.php
@@ -0,0 +1,18 @@
+<?php 
+
+/* 
+    Don't Remove this comment!. 
+    
+    Check if given message is spam or not
+    Input: a string 
+    Output: return true if spam/offensive,else return false 
+    Condition: The comment should not condition any offensive words or spamy. Assume the message string is in english.
+    
+    Your can create as many as auxillary functions you need. 
+  
+*/ 
+
+
+// Write your php fucntion below
+
+?>
