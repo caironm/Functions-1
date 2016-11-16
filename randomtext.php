0baf43a419ed9571ea93c8e15227316983a523f2
Monish Raj <rajwithgilly97@gmail.com>
2016-11-16 22:25:19 +0530

    Update randomtext.php

--- randomtext.php
+++ randomtext.php
@@ -6,6 +6,18 @@
   Input Params: n - length of output string (Default: 20)
   Conditions: Output String Must Only Contain a-z, A-Z, 0-9. No Special Chars.
 */
+<?php
+function RandomString()
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randstring = '';
+    for ($i = 0; $i < 20; $i++) {
+        $randstring = $characters[rand(0, strlen($characters))];
+    }
+    return $randstring;
+}
+RandomString();
+echo $randstring;
 
 
 
