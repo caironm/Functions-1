77b45e13d21afa833de17508b51fe58582c25435
Monish Raj <rajwithgilly97@gmail.com>
2016-11-16 22:49:54 +0530

    Update recaptcha.php

--- recaptcha.php
+++ recaptcha.php
@@ -12,4 +12,9 @@
 */
 // Write your php fucntion below
 
+          require_once('recaptchalib.php');
+          $publickey = "your_public_key"; // you got this from the signup page
+          echo recaptcha_get_html($publickey);
+      
+
 ?>
